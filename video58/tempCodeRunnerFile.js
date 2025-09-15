e6a098890d3e072009a71dc3f548f76b620342a2
2205158@cse.buet.ac.bd <140476865+nihal6251@users.noreply.github.com>
2025-09-15 18:43:29 +0600

    Publishing to github

---
+++ video58/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log("The sum of these numbers is: ", result1)
+console.log("The sum of these numbers is: ", result2)
+console.log("The sum of these numbers is: ", result3)
---
+++ video59/index.js
@@ -0,0 +1,1 @@
+//Creat a faulty calculator using js
