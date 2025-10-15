a1fc5f27ca40c5709f09ef675c92b7217c7ca067
nihal6251 <your_email_used_in_github@example.com>
2025-10-16 03:18:44 +0600

    Add basic canvas drawing example
    Introduced a simple HTML, CSS, and JavaScript setup for a canvas crash course. The example demonstrates drawing filled and stroked rectangles on a canvas, with basic styling for layout and appearance.

---
+++ canvas/script.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//fillRect()
+ctx.fillStyle = 'red';
+ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle = 'blue';
+ctx.fillRect(200, 20, 150, 100);
+
+// strokeRect()
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'green';
+ctx.strokeRect(100, 200, 150, 100);
+
+// clearRect()
+ctx.clearRect(25, 25, 140, 9 0);
